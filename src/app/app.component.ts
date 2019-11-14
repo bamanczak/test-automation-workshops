05b5830d0a021c6def30ee126e67ef00c3740b89
Wojciech Matuszewski <wm.matuszewski@wp.pl>
2019-11-14 09:08:55 +0100

    fix lint warnings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,29 @@
-import {
-  Component,
-  OnInit
-} from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 import {
   trigger,
   state,
   style,
   animate,
   transition
-} from '@angular/animations';
-
+} from "@angular/animations";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
   animations: [
-    trigger(
-      'inOutAnimation',
-      [
-        transition(
-          ':enter',
-          [
-            style({ opacity: 0 }),
-            animate('1s 0.35s ease-out',
-                    style({ opacity: 1 }))
-          ]
-        ),
-        transition(
-          ':leave',
-          [
-            style({ opacity: 1 }),
-            animate('0.3s ease-in',
-                    style({ opacity: 0 }))
-          ]
-        )
-      ]
-    )
+    trigger("inOutAnimation", [
+      transition(":enter", [
+        style({ opacity: 0 }),
+        animate("1s 0.35s ease-out", style({ opacity: 1 }))
+      ]),
+      transition(":leave", [
+        style({ opacity: 1 }),
+        animate("0.3s ease-in", style({ opacity: 0 }))
+      ])
+    ])
   ]
 })
-
 export class AppComponent {
   inputNumber: string;
   isValid: boolean;
@@ -50,7 +34,6 @@ export class AppComponent {
   isMale;
   isInitiated: boolean;
   hasFocus: boolean;
-
 
   onSubmit() {
     this.isInitiated = true;
@@ -63,16 +46,22 @@ export class AppComponent {
   }
 
   isValidPesel(pesel) {
-    if (typeof pesel !== 'string') {
+    if (typeof pesel !== "string") {
       return false;
     } else {
       const peselArray = new Array();
       for (let i = 0; i < 11; i++) {
-        peselArray[i] = parseInt(pesel.substring(i, i + 1));
+        peselArray[i] = parseInt(pesel.substring(i, i + 1), 10);
       }
       this.peselArray = peselArray;
-      return this.verifyControlNumber(peselArray) &&
-              this.verifyDate(this.getYear(peselArray), this.getMonth(peselArray), this.getDay(peselArray));
+      return (
+        this.verifyControlNumber(peselArray) &&
+        this.verifyDate(
+          this.getYear(peselArray),
+          this.getMonth(peselArray),
+          this.getDay(peselArray)
+        )
+      );
     }
   }
 
@@ -81,7 +70,7 @@ export class AppComponent {
     let sum = 0;
     const controlNumber = pesel[10];
     for (let i = 0; i < weight.length; i++) {
-      sum += (pesel[i] * weight[i]);
+      sum += pesel[i] * weight[i];
     }
     sum = sum % 10;
     return sum === controlNumber;
@@ -95,9 +84,9 @@ export class AppComponent {
 
   verifyDate(y: number, m: number, d: number) {
     const dt = new Date(y, m - 1, d);
-    return dt.getDate() === d &&
-          dt.getMonth() === m - 1 &&
-          dt.getFullYear() === y;
+    return (
+      dt.getDate() === d && dt.getMonth() === m - 1 && dt.getFullYear() === y
+    );
   }
 
   getMonth(pesel: Array<number>) {
@@ -121,5 +110,4 @@ export class AppComponent {
 
     return year;
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { ReactiveFormsModule } from '@angular/forms';
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ReactiveFormsModule } from "@angular/forms";
+import { FlexLayoutModule } from "@angular/flex-layout";
 
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from "@angular/material/button";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
 
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 @NgModule({
   declarations: [
